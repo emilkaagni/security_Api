970418f61c88ed65b4d394cba2a8f43b8c8380a2
Emil Basnyat <emilbasnyat@Emils-MacBook-Pro.local>
2025-02-01 12:35:00 +0545

    final

--- model/auditLog.js
+++ model/auditLog.js
@@ -1,36 +1,36 @@
 const mongoose = require("mongoose");
-
+// logger
 const auditLogSchema = new mongoose.Schema({
-    userId: {
-        type: mongoose.Schema.Types.Mixed, 
-        ref: "users",
-        required: true,
-    },
-    operation: {
-        type: String,
-        enum: ["create", "update", "delete", "login"],
-        required: true,
-    },
-    collectionName: {
-        type: String,
-        required: true,
-    },
-    documentId: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: false,
-    },
-    oldValue: {
-        type: mongoose.Schema.Types.Mixed,
-        required: false,
-    },
-    newValue: {
-        type: mongoose.Schema.Types.Mixed,
-        required: false,
-    },
-    timestamp: {
-        type: Date,
-        default: Date.now,
-    },
+  userId: {
+    type: mongoose.Schema.Types.Mixed,
+    ref: "users",
+    required: true,
+  },
+  operation: {
+    type: String,
+    enum: ["create", "update", "delete", "login"],
+    required: true,
+  },
+  collectionName: {
+    type: String,
+    required: true,
+  },
+  documentId: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: false,
+  },
+  oldValue: {
+    type: mongoose.Schema.Types.Mixed,
+    required: false,
+  },
+  newValue: {
+    type: mongoose.Schema.Types.Mixed,
+    required: false,
+  },
+  timestamp: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 const AuditLog = mongoose.model("AuditLog", auditLogSchema);
--- routes/logRoutes.js
+++ routes/logRoutes.js
@@ -1,17 +1,3 @@
-// const express = require("express");
-// const { getLogs } = require("../controller/logController");
-// const { protect, isAdmin } = require("../middleware/auth");
-
-// const router = express.Router();
-
-// // Protect all routes and require admin access
-// router.use(protect);
-// router.use(isAdmin);
-
-// router.get("/", getLogs);
-
-// module.exports = router;
-
 const express = require("express");
 const { getLogs } = require("../controller/logController");
 const { protect, isAdmin } = require("../middleware/auth");
