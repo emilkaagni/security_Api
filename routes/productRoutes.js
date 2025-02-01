86a2285c3b456ff505ed75e300a3a40eca29f554
Emil Basnyat <emilbasnyat@Emils-MacBook-Pro.local>
2025-02-01 17:44:10 +0545

    all

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,38 +1,3 @@
-// const router = require('express').Router();
-// const productController = require('../controllers/productController')
-// const authMiddleware = require('../middleware/authGuard');
-
-
-
-// router.post('/create', authMiddleware.authGuard, productController.createProduct)
-
-// // fetch all products
-// // router.get('/get_all_products', productController.getAllProducts)
-// router.get('/get_all_products', authMiddleware.authGuard, productController.getAllProducts);
-
-// // fetch all products for User
-// // router.get('/get_all_products', productController.getAllProducts)
-// // router.get('/get_all_products_user', authMiddleware.authGuard, productController.getAllProductsUser);
-
-// // Single product
-// router.get('/get_single_product/:id', productController.getSingleProduct)
-
-// // delete product
-// router.delete('/api/product/delete_product/:id', productController.deleteProduct)
-
-// // update product
-// router.put('/api/product/update_product/:id', productController.updateProduct)
-
-// // pagination
-// router.get('/pagination', productController.paginationProducts)
-// module.exports = router
-
-
-
-
-
-
-
 const router = require('express').Router();
 const productController = require('../controllers/productController');
 const authMiddleware = require('../middleware/authGuard');
