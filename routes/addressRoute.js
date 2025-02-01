4f7e088e7aaa4d5e2f7e23d4303da00914ac4233
Emil Basnyat <emilbasnyat@Emils-MacBook-Pro.local>
2025-02-01 12:35:00 +0545

    final

--- routes/addressRoute.js
+++ routes/addressRoute.js
@@ -1,14 +1,35 @@
 // module.exports = router;
-const express = require('express');
+// aaa
+const express = require("express");
 const router = express.Router();
-const shippingAddressController = require('../controller/addressController');
-const { authGuard } = require('../middleware/auth');
+const shippingAddressController = require("../controller/addressController");
+const { authGuard } = require("../middleware/auth");
 
 // Create or Update Shipping Address
-router.post('/shipping-address', authGuard, shippingAddressController.createShippingAddress);
-router.put('/update-shipping-address/:addressId', authGuard, shippingAddressController.updateShippingAddress);
-router.get('/getaddress/:userId', authGuard, shippingAddressController.getAllShippingAddresses);
-router.delete('/deleteaddress/:addressId', authGuard, shippingAddressController.deleteAddress);
-router.get('/getaddressbyaddressId/:addressId', authGuard, shippingAddressController.getShippingAddressById);
+router.post(
+  "/shipping-address",
+  authGuard,
+  shippingAddressController.createShippingAddress
+);
+router.put(
+  "/update-shipping-address/:addressId",
+  authGuard,
+  shippingAddressController.updateShippingAddress
+);
+router.get(
+  "/getaddress/:userId",
+  authGuard,
+  shippingAddressController.getAllShippingAddresses
+);
+router.delete(
+  "/deleteaddress/:addressId",
+  authGuard,
+  shippingAddressController.deleteAddress
+);
+router.get(
+  "/getaddressbyaddressId/:addressId",
+  authGuard,
+  shippingAddressController.getShippingAddressById
+);
 
 module.exports = router;
